99b148299fc269f3692ea55cfb931e6c1e6d441e
mirahmadian <153491033+mirahmadian@users.noreply.github.com>
2026-05-29 19:47:43 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,11 @@
-const CACHE = 'iraq-guide-v1';
+const CACHE = 'iraq-guide-v2';
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
   './icon.svg',
+  './app-icon.png',
+  './hajj-logo.png',
   'https://fonts.googleapis.com/css2?family=Vazirmatn:wght@300;400;500;600;700;800;900&display=swap',
   'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js'
 ];
@@ -17,7 +19,9 @@ self.addEventListener('install', e => {
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys()
-      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(keys => Promise.all(keys.map(k => {
+        if (k !== CACHE) return caches.delete(k);
+      })))
       .then(() => self.clients.claim())
   );
 });
